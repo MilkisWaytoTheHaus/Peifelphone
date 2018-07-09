1026174ecf291ec213b390f92f5a513543e55330
Salomonelle <dochdoch@gmx-ist-cool.de>
2018-07-09 14:47:18 +0200

    Add files via upload

---
+++ serialWrite.py
@@ -0,0 +1,24 @@
+ #!/usr/bin/env python
+          
+      
+import time
+import serial
+
+
+ser = serial.Serial(
+  
+   port='/dev/ttyS0',
+   baudrate = 9600,
+   parity=serial.PARITY_NONE,
+   stopbits=serial.STOPBITS_ONE,
+   bytesize=serial.EIGHTBITS,
+   timeout=1
+)
+counter=0
+
+
+while 1:
+   inp = raw_input("Send command: ")
+   ser.write(inp)
+   time.sleep(1)
+   counter += 1
